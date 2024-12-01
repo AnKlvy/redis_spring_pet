ef6ad34fc08f5bcc246094f37f1868282789e5ee
Andrey Klimov <andreyklimov.ver2@gmail.com>
2024-12-01 11:03:37 +0500

    added saving seesion in redis

--- src/main/java/com/example/redis_spring/RedisSpringApplication.java
+++ src/main/java/com/example/redis_spring/RedisSpringApplication.java
@@ -1,13 +1,25 @@
 package com.example.redis_spring;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class RedisSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RedisSpringApplication.class, args);
 	}
+	@GetMapping("/")
+	public String hello() {
+		return "hello";
+	}
 
+	@GetMapping("/session")
+	public String getSession(HttpSession session) {
+		return "session: " + session;
+	}
 }
