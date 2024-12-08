13d6e373885d9212e11c705d7bf35b5d92dfd36b
Andrey Klimov <andreyklimov.ver2@gmail.com>
2024-12-09 03:24:20 +0500

    finished monitoring of cache

--- src/main/java/com/example/redis_spring/services/BookService.java
+++ src/main/java/com/example/redis_spring/services/BookService.java
@@ -9,9 +9,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-
 import java.util.Optional;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -20,14 +19,12 @@ public class BookService {
     private final BookRepository bookRepository;
     private final CacheService cacheService;
 
-
-
     public Book getBook(Long id) {
         final String cacheKey = "book:" + id;
-        log.info("Deleting cache for popular books before retrieving book...");
+        log.info("Attempting to delete cache for popular books before retrieving book...");
         cacheService.deleteCachedObject("popular_books");
 
-        // Проверяем кэш книги
+        log.info("Checking cache for book with ID: {}", id);
         Book cachedBook = cacheService.getCachedObject(cacheKey, new TypeReference<>() {});
         if (cachedBook != null) {
             log.info("Book found in cache: {}", cachedBook);
@@ -37,10 +34,11 @@ public class BookService {
             return cachedBook;
         }
 
-        log.info("Book not found in cache. Querying database...");
+        log.info("Book not found in cache. Querying database for book with ID: {}", id);
         Optional<Book> bookOptional = bookRepository.findById(id);
         if (bookOptional.isPresent()) {
             Book book = bookOptional.get();
+            log.info("Book found in database: {}", book);
             book.setPopularity(book.getPopularity() + 1);
             bookRepository.save(book);
             cacheService.cacheObject(cacheKey, book, 1, TimeUnit.MINUTES);
@@ -51,41 +49,38 @@ public class BookService {
         return null;
     }
 
-
-
     public void updateBook(Book book) {
-        // Обновляем продукт в базе данных
+        log.info("Updating book in database: {}", book);
         bookRepository.save(book);
 
-        // Обновляем продукт в кэше
+        log.info("Caching updated book with ID: {}", book.getId());
         cacheService.cacheObject("book:" + book.getId(), book, 1, TimeUnit.MINUTES);
     }
 
     public void deleteBook(Long bookId) {
-        // Удаляем продукт из базы данных
+        log.info("Deleting book with ID: {}", bookId);
         bookRepository.deleteById(bookId);
 
-        // Удаляем продукт из кэша
+        log.info("Deleting book from cache with key: {}", "book:" + bookId);
         cacheService.deleteCachedObject("book:" + bookId);
     }
 
     public List<Book> getPopularBooks() {
         final String cacheKey = "popular_books";
 
-        // Пробуем получить популярные книги из кэша
+        log.info("Checking cache for popular books...");
         List<Book> cachedBooks = cacheService.getCachedObject(cacheKey, new TypeReference<>() {});
         if (cachedBooks != null) {
+            log.info("Popular books found in cache.");
             return cachedBooks;
         }
 
-        // Если в кэше нет, получаем из базы данных
+        log.info("Popular books not found in cache. Querying database for popular books...");
         List<Book> popularBooks = bookRepository.findPopularBooks();
 
-        // Кэшируем результат
+        log.info("Caching popular books with key: {}", cacheKey);
         cacheService.cacheObject(cacheKey, popularBooks, 5, TimeUnit.MINUTES);
 
         return popularBooks;
     }
-
-
 }
