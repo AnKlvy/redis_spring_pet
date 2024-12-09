a9b3d279fd108daa5ba15126a872f1e3924e1783
Andrey Klimov <andreyklimov.ver2@gmail.com>
2024-12-09 05:49:35 +0500

    finished redis with sessions for user

---
+++ src/main/java/com/example/redis_spring/controllers/LogoutController.java
@@ -0,0 +1,27 @@
+package com.example.redis_spring.controllers;
+
+import com.example.redis_spring.services.CacheService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.session.Session;
+import org.springframework.session.SessionRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class LogoutController {
+    private final CacheService cacheService;
+
+    @Autowired
+    private SessionRepository<? extends Session> sessionRepository;
+
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        String sessionId = request.getSession().getId();
+        cacheService.deleteCachedObject(sessionId);  // Удаляем сессию из Redis
+        return "redirect:/login?logout";
+    }
+}
---
+++ src/main/java/com/example/redis_spring/services/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.example.redis_spring.services;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Устанавливаем фиксированное имя и пароль
+        return User.builder()
+                .username("AnKv")  // Фиксированное имя
+                .password("{noop}AnKv")  // Фиксированный пароль (noop — без кодирования)
+                .roles("admin")  // Роль пользователя
+                .build();
+    }
+}
